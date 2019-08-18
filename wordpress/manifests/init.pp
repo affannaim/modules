6b49725e79f9a4562faa7ec7ecbbc6109c688ef7
root <root@puppetworkstation.us-central1-a.c.speedy-cab-246006.internal>
2019-08-18 04:53:00 +0000

    updated code

--- wordpress/manifests/init.pp
+++ wordpress/manifests/init.pp
@@ -1,8 +1,8 @@
-class wordpress {
+#class wordpress {
 #sudo apt update
 exec {'apt update':
  path => "/usr/bin",
- command => sudo apt update,
+ command => "apt update",
 }
 
 #sudo apt install -y apache2
@@ -11,40 +11,53 @@ package {"apache2":
 }
 
 #sudo service apache2 start
-service {"apache2":
- ensure => start,
+service {"apache2": 
+ ensure => running,
 }
 
 #sudo apt install -y mysql-server mysql-client
 #sudo apt install -y php libapache2-mod-php php-mcrypt php-mysql
-$pkg = ['mysql-server', 'mysql-client', 'php', 'libapache2-mod-php', 'php-mcrypt', 'php-mysql']
-$pkg.each |string packg| {
+$pkg = ['php', 'libapache2-mod-php', 'php-mcrypt', 'php-mysql']
+$pkg.each |String $packg| { 
 package {"${packg}":
  ensure => present,
 }
+}
 
 #mysqladmin -u root password rootpassword
-exec {'mysqladmin':
- command => mysqladmin -u root password rootpassword,
-}
+#exec {'mysqladmin':
+# command => "mysqladmin -u root password rootpassword",
+#}
 
 #wget https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/chefwordpress/files/default/mysqlcommands
 #sudo cp mysqlcommands /tmp/mysqlcommands
-exec {"wget https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/chefwordpress/files/default/mysqlcommands":
- path => "/usr/bin",
- cwd =>"/tmp/mysqlcommands",
-}
+#exec {"wget https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/chefwordpress/files/default/mysqlcommands":
+# path => "/usr/bin",
+# cwd =>"/tmp/mysqlcommands",
+#}
 
 #mysql -uroot -prootpassword < /tmp/mysqlcommands
-exec {'mysqlcommand':
- command => mysql -uroot -prootpassword < /tmp/mysqlcommands,
-}
+#exec {'mysqlcommand':
+# command => mysql -uroot -prootpassword < /tmp/mysqlcommands,
+#}
 
 #wget https://wordpress.org/latest.zip
 #cp latest.zip /tmp/latest.zip
+
+class { '::mysql::server':
+  root_password           => 'rootpassword',
+}
+
+mysql::db { 'wordpress':
+  user     => 'wordpressuser',
+  password => 'password',
+  host     => 'localhost',
+  grant    => ['ALL'],
+}
+
 exec {"wget https://wordpress.org/latest.zip":
  path => "/usr/bin",
- cwd =>"/tmp/latest.zip",
+ cwd => "/tmp",
 }
 
 #sudo apt install -y unzip
@@ -54,26 +67,30 @@ package {"unzip":
 
 #sudo unzip /tmp/latest.zip -d /var/www/html
 exec {'unzip latest.zip':
- command => sudo unzip /tmp/latest.zip -d /var/www/html
+ command => "/usr/bin/unzip /tmp/latest.zip -d /var/www/html"
 }
 
 #wget https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/chefwordpress/files/default/wp-config-sample.php
 #sudo cp wp-config-sample.php /var/www/html/wordpress/wp-config.php
 exec {"wget https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/chefwordpress/files/default/wp-config-sample.php":
  path => "/usr/bin",
- cwd =>"/var/www/html/wordpress/wp-config.php",
+ cwd => "/var/www/html/wordpress",
 }
 
 #sudo chmod -R 775 /var/www/html/wordpress
 #sudo chown -R www-data:www-data /var/www/html/wordpress
 file {'/var/www/html/wordpress':
- mode => '775'
- owner => 'www-data'
- group => 'www-data'
+ mode => '775',
+ owner => 'www-data',
+ group => 'www-data',
 }
 
 #sudo service apache2 restart
-service {"apache2":
- ensure :restart,
+#service {"apache2_restart":
+# name => "apache2", 
+# ensure => restart,
+#}
+exec {'exec apache2 restart':
+ command => "/usr/sbin/service apache2 restart",
 }
-}
+#}
